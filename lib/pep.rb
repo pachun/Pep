5c6e2661ddb25bc832d916cc13259ac3037ab2e6
Nick Pachulski <nick@pachulski.me>
2023-03-14 16:12:35 -0400

    Update the default App.tsx content
    The new content gives new app creators some direction on how to get
    started developing once they’ve already run `pep new-app-name`.

--- lib/pep.rb
+++ lib/pep.rb
@@ -390,16 +390,27 @@ class InstallReactNavigation
       const [homeScreensTextInputValue, setHomescreensTextInputValue] =
         useState<string>("")
       return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-          <Text>Home Screen</Text>
+        <View style={{ flex: 1, alignItems: "center" }}>
+          <Text style={{ fontWeight: "bold", marginTop: 100 }}>
+            Open `src/App.tsx` to begin editing your app.
+          </Text>
+          <Text style={{ marginTop: 100 }}>Home Screen</Text>
           <TextInput
             value={homeScreensTextInputValue}
             onChangeText={setHomescreensTextInputValue}
-            placeholder="Type somethin here dawg"
-            style={{ width: "50%", height: 50 }}
+            placeholder="Type something here to see it on the details screen"
+            style={{
+              width: "90%",
+              height: 30,
+              textAlign: "center",
+              borderWidth: 1,
+              borderColor: "#aaa",
+              marginTop: 20,
+              marginBottom: 20,
+            }}
           />
           <Button
-            title="Go to Details"
+            title="Go to Details Screen"
             onPress={(): void => {
               navigation.navigate("Details", { homeScreensTextInputValue })
             }}
